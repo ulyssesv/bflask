c9596e7810e5dd48a3726077a53aa6237f045ead
Ulysses V <ulysses@hub9.co>
2016-06-02 02:00:04 -0300

    Adding initial app and models

---
+++ README.md
@@ -0,0 +1,45 @@
+# Busbus
+
+An API proxy for NextBus departure information.
+
+This app fetches routes and stops information from the NextBus public API and
+stores it locally in a database. It also provides departure information for the
+closest stops of a desired location.
+
+## Development Environment
+
+At a bare minimum you'll need:
+
+- Python 3
+- PostgreSQL
+- Virtualenv
+
+Assuming you have the required dependencies, then run:
+
+```bash
+$ git clone <repo>
+$ cd <project>
+$ mkdir env | virtualenv env --python python3
+$ source env/bin/activate
+$ pip install -r requirements.txt
+```
+
+The app uses a `.env` file to export environment variables to its instance
+settings. Create a `.env` file like the sample below and modify according to
+your local setup.
+
+```
+FLASK_DEBUG=1
+DATABASE_URL=postgresql://user:password@host:port/database
+```
+
+The app uses [Alembic](https://bitbucket.org/zzzeek/alembic) to manage database
+migrations. Run `$ python app.py db upgrade` to install.
+
+## Running
+
+`TODO`
+
+## Testing
+
+`TODO`
---
+++ app.py
@@ -0,0 +1,54 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', None)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+class Agency(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(20), unique=True, index=True)
+    title = db.Column(db.Unicode(100))
+    routes = db.relationship('Route', backref='agency', lazy='dynamic')
+
+class Route(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    agency_id = db.Column(db.Integer, db.ForeignKey('agency.id'))
+    tag = db.Column(db.String(20), index=True)
+    title = db.Column(db.Unicode(100))
+
+route_stop = db.Table('route_stop',
+    db.Column('route_id', db.Integer, db.ForeignKey('route.id')),
+    db.Column('stop_id', db.Integer, db.ForeignKey('stop.id'))
+)
+
+class Stop(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(20), index=True)
+    external_id = db.Column(db.Integer)
+    latitude = db.Column(db.Numeric(10, 7), index=True)
+    longitude = db.Column(db.Numeric(10, 7), index=True)
+    title = db.Column(db.Unicode(100))
+    routes = db.relationship('Route', secondary=route_stop, backref=db.backref('stops', lazy='dynamic'))
+
+@manager.command
+def sync():
+    """Syncs with NextBus API."""
+    pass
+
+
+if __name__ == '__main__':
+    manager.run()
